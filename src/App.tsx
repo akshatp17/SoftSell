c4211088affbad9ffcfe2db186f776086cfbbfd4
Akshat Pratyush <akshatp0505@gmail.com>
2025-05-16 00:48:29 +0530

    basic files to use

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
+import Navbar from "./components/Navbar";
+
 const App = () => {
-  return <div></div>;
+  return (
+    <>
+      <Navbar />
+    </>
+  );
 };
 
 export default App;
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,5 @@
+const Navbar = () => {
+  return <div className="w-full bg-amber-200">SoftSell</div>;
+};
+
+export default Navbar;
